2d9acad743fed15f2ea98d4250db4fb7f329f4f7
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-11 14:02:44 -0500

    A need for generic types. Implementing a generic type

--- Generic types & advanced use of methods/Program.cs
+++ Generic types & advanced use of methods/Program.cs
@@ -1,4 +1,4 @@
-﻿var numbers = new ListOfInts();
+﻿var numbers = new SimpleList<int>();
 numbers.Add(10);
 numbers.Add(20);
 numbers.Add(30);
@@ -7,8 +7,69 @@ numbers.Add(50);
 
 numbers.RemoveAt(2);
 
+var words = new SimpleList<string>();
+words.Add("aaa");
+words.Add("bbb");
+words.Add("ccc");
+
+var dates = new SimpleList<DateTime>();
+dates.Add(new DateTime(2025,1,6));
+dates.Add(new DateTime(2025, 1, 3));
+
+
 Console.ReadKey();
 
+
+class SimpleList<T>
+{
+	private T[] _items = new T[4];
+	private int _size = 0;
+
+	public void Add(T item)
+	{
+		if (_size >= _items.Length)
+		{
+			var newItems = new T[_items.Length * 2];
+
+			for (int i = 0; i < _items.Length; i++)
+			{
+				newItems[i] = _items[i];
+			}
+			_items = newItems;
+		}
+		_items[_size] = item;
+		++_size;
+	}
+
+	public void RemoveAt(int index)
+	{
+		if (index < 0 || index > _size)
+		{
+			throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+		}
+
+		--_size;
+
+		for (int i = index; i < _size; ++i)
+		{
+			_items[i] = _items[i + 1];
+		}
+
+		_items[_size] = default(T);
+
+
+	}
+
+	public T GetAtIndex(int index)
+	{
+		if (index < 0 || index >= _size)
+		{
+			throw new IndexOutOfRangeException($"Index {index} is outside the bounds of the list.");
+		}
+
+		return _items[index];
+	}
+}
 
 class ListOfInts
 {
@@ -58,3 +119,4 @@ class ListOfInts
 		return _items[index];
 	}
 }
+
