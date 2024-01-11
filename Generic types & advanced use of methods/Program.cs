81240f88f37358bb35d30d5783c6076b9330b8bd
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-11 11:28:33 -0500

    Simplified List (adding a new item)

--- Generic types & advanced use of methods/Program.cs
+++ Generic types & advanced use of methods/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var numbers = new ListOfInts();
+numbers.Add(10);
+numbers.Add(20);
+numbers.Add(30);
+numbers.Add(40);
+numbers.Add(50);
+
+Console.ReadKey();
+
+
+class ListOfInts
+{
+	private int[] _items = new int[4];
+	private int _size = 0;
+
+	public void Add(int item)
+	{
+		if (_size >= _items.Length)
+		{
+			var newItems = new int[_items.Length * 2];
+
+			for (int i = 0; i < _items.Length; i++)
+			{
+				newItems[i] = _items[i];
+			}
+			_items = newItems;
+		}
+		_items[_size] = item;
+		++_size;
+	}
+}
