73dc4d7373c58988a5cc6d1c8cd3b952c115ac2a
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-17 13:07:50 -0500

    Type constraints – numeric types. Generic math

--- Generic types & advanced use of methods/Program.cs
+++ Generic types & advanced use of methods/Program.cs
@@ -377,46 +377,64 @@
 //}
 
 
-var john = new Person { Name = "John", YearOfBirth = 1980 };
-var anna = new Person { Name = "Anna", YearOfBirth = 1915 };
+//var john = new Person { Name = "John", YearOfBirth = 1980 };
+//var anna = new Person { Name = "Anna", YearOfBirth = 1915 };
 
-PrintInOrder(10, 5);
-PrintInOrder("aaa", "bb");
-PrintInOrder(anna, john);
+//PrintInOrder(10, 5);
+//PrintInOrder("aaa", "bb");
+//PrintInOrder(anna, john);
+
+//Console.ReadKey();
+
+//void PrintInOrder<T>(T first, T second) where T : IComparable<T>
+//{
+//	if (first.CompareTo(second) > 0)
+//	{
+//		Console.WriteLine($"{second} {first}");
+//	}
+//	else
+//	{
+//		Console.WriteLine($"{first} {second}");
+//	}
+//}
+
+//public class Person : IComparable<Person>
+//{
+
+//	public string Name { get; init; }
+//	public int YearOfBirth { get; init; }
+
+//	public override string ToString() => $"{Name} born in {YearOfBirth}";
+
+//	public int CompareTo(Person other)
+//	{
+//		if (YearOfBirth < other.YearOfBirth)
+//		{
+//			return 1;
+//		}
+//		else if (YearOfBirth > other.YearOfBirth)
+//		{
+//			return -1;
+//		}
+
+//		return 0;
+//	}
+//}
+
+
+
+
+using System.Numerics;
+
+Console.WriteLine("Square of 2 is: " + Calculator.Square(2));
+Console.WriteLine("Square of 4m is: " + Calculator.Square(4m));
+Console.WriteLine("Square of 6d is: " + Calculator.Square(6d));
 
 Console.ReadKey();
 
-void PrintInOrder<T>(T first, T second) where T : IComparable<T>
+
+public static class Calculator
 {
-	if (first.CompareTo(second) > 0)
-	{
-		Console.WriteLine($"{second} {first}");
-	}
-	else
-	{
-		Console.WriteLine($"{first} {second}");
-	}
-}
+	public static T Square<T>(T input) where T : INumber<T> => input * input;
 
-public class Person : IComparable<Person>
-{
-
-	public string Name { get; init; }
-	public int YearOfBirth { get; init; }
-
-	public override string ToString() => $"{Name} born in {YearOfBirth}";
-
-	public int CompareTo(Person other)
-	{
-		if (YearOfBirth < other.YearOfBirth)
-		{
-			return 1;
-		}
-		else if (YearOfBirth > other.YearOfBirth)
-		{
-			return -1;
-		}
-
-		return 0;
-	}
 }
